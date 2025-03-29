c7febcb3e8c23404adc18c4dee837a7ad074d1cf
Ayushman <ayushmandevnath2006@gmail.com>
2025-03-30 03:13:38 +0530

    changed eslintrc.js

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,10 +1,10 @@
-export default {
+module.exports = {
   env: {
     es6: true,
     node: true,
   },
   parserOptions: {
-    "ecmaVersion": 2018,
+    ecmaVersion: 2018,
   },
   extends: [
     "eslint:recommended",
@@ -13,7 +13,7 @@ export default {
   rules: {
     "no-restricted-globals": ["error", "name", "length"],
     "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    "quotes": ["error", "double", { "allowTemplateLiterals": true }],
   },
   overrides: [
     {
