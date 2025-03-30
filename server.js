33c01b4ee0d0fcd48848f1cc0e8f1f798f27b188
Ayushman <ayushmandevnath2006@gmail.com>
2025-03-30 23:57:19 +0530

    Final changes

--- server.js
+++ server.js
@@ -111,7 +111,11 @@ app.use(express.json());
 //     });
 // }
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/logo.ico", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "logo.ico"));
+});
 
 app.post('/api/recommendations', async (req, res) => {
     const moodScores = req.body;
